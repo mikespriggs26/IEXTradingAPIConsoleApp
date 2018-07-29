feefa70b3e1fb83217d182c4895f915a3ebdaef5
mikespriggs26 <mikespriggs@msn.com>
2018-07-29 15:35:59 -0400

    Final product

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -12,36 +12,10 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            //var symbol = "msft";
-            //var IEXTrading_API_PATH = "https://api.iextrading.com/1.0/stock/{0}/chart/1y";
-
-            //IEXTrading_API_PATH = string.Format(IEXTrading_API_PATH, symbol);
-
-            //using (HttpClient client = new HttpClient())
-            //{
-            //    client.DefaultRequestHeaders.Accept.Clear();
-            //    client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-
-            //    //For IP-API
-            //    client.BaseAddress = new Uri(IEXTrading_API_PATH);
-            //    HttpResponseMessage response = client.GetAsync(IEXTrading_API_PATH).GetAwaiter().GetResult();
-            //    if (response.IsSuccessStatusCode)
-            //    {
-            //        var historicalDataList = response.Content.ReadAsAsync<List<HistoricalDataResponse>>().GetAwaiter().GetResult();
-            //        foreach (var historicalData in historicalDataList)
-            //        {
-            //            if (historicalData != null)
-            //            {
-            //                Console.WriteLine("Open: " + historicalData.open);
-            //                Console.WriteLine("Close: " + historicalData.close);
-            //                Console.WriteLine("Low: " + historicalData.low);
-            //                Console.WriteLine("High: " + historicalData.high);
-            //                Console.WriteLine("Change: " + historicalData.change);
-            //                Console.WriteLine("Change Percentage: " + historicalData.changePercent);
-            //            }
-            //        }
-            //    }
-            var symbol = "msft";
+            //  This console application uses the IEX Trading API to retrieve stock data and market data
+            //
+            Console.WriteLine("Please enter a ticker symbol: ");
+            var symbol = Console.ReadLine();
             var IEXTrading_API_PATH = "https://api.iextrading.com/1.0/stock/{0}/company";
 
             IEXTrading_API_PATH = string.Format(IEXTrading_API_PATH, symbol);
@@ -54,28 +28,26 @@ namespace ConsoleApp2
                 //For IP-API
                 client.BaseAddress = new Uri(IEXTrading_API_PATH);
                 HttpResponseMessage response = client.GetAsync(IEXTrading_API_PATH).GetAwaiter().GetResult();
-                //string myResponse = response;
+               
 
                 if (response.IsSuccessStatusCode)
                 {
                     var historicalDataList = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
-                    Console.WriteLine(historicalDataList);
+                    
                     string myResult = historicalDataList.ToString();
-                    Console.WriteLine(myResult);
+                    
                     CompanyResponse item = Newtonsoft.Json.JsonConvert.DeserializeObject<CompanyResponse>(myResult);
-                    Console.WriteLine(item.companyName);
-                    //CompanyResponse[] items = JsonConvert.DeserializeObject<CompanyResponse[]>(myResult);
-
-                    //foreach (var historicalData in items)
-                    //{
-                    //    if (historicalDataList != null)
-                    //    {
-                    //        Console.WriteLine(historicalData);
-
-
-                            Console.ReadKey();
-                    //    }
-                    //}
+                    Console.WriteLine("Company name: " + item.companyName);
+                    Console.WriteLine("Symbol: " + item.symbol);
+                    Console.WriteLine("Exchange: " + item.exchange);
+                    Console.WriteLine("Industry: " + item.industry);
+                    Console.WriteLine("Website: " + item.website);
+                    Console.WriteLine("CEO: " + item.CEO);
+                    Console.WriteLine("Issue Type: " + item.issueType);
+                    Console.WriteLine("Sector: " + item.sector);
+                    Console.WriteLine("Description: " + item.description);
+                    Console.ReadKey();
+                    
                 }
 
 
