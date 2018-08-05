3225c2ee2a524e77db2b2a0abeb43ad78278dcc2
mikespriggs26 <mikespriggs@msn.com>
2018-08-04 20:44:36 -0400

    Adding changes

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -46,28 +46,17 @@ namespace ConsoleApp2
                     Console.WriteLine("Issue Type: " + item.issueType);
                     Console.WriteLine("Sector: " + item.sector);
                     Console.WriteLine("Description: " + item.description);
-                    Console.ReadKey();
+                    
                     
                 }
 
 
             }
+            
+
+            
         }
-        public class HistoricalDataResponse
-        {
-            public string date { get; set; }
-            public double open { get; set; }
-            public double high { get; set; }
-            public double low { get; set; }
-            public double close { get; set; }
-            public int volume { get; set; }
-            public int unadjustedVolume { get; set; }
-            public double change { get; set; }
-            public double changePercent { get; set; }
-            public double vwap { get; set; }
-            public string label { get; set; }
-            public double changeOverTime { get; set; }
-        }
+       
         public class StockPriceResponse
         {
             public double price { get; set; }
